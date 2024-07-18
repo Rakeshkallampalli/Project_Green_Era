f01c55e69b884a227858198f9af09238a2642c65
unknown <rakeshkallampalli@gmail.com>
2024-07-17 21:04:41 -0400

    view for adding a Rating for product

--- Green_era/EcoFriendlyProduct/static/views.py
+++ Green_era/EcoFriendlyProduct/static/views.py
@@ -11,25 +11,42 @@ from django.http import JsonResponse
 from django.shortcuts import redirect, get_object_or_404
 from django.shortcuts import render
 from django.utils.decorators import method_decorator
+from django.views import View
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 
 
 from django.db.models import Sum
 
+from Green_era.EcoFriendlyProduct.models import User
 
-class UserRegistrationForm:
+
+class Product:
     pass
 
 
-def register(request):
-    if request.method == 'POST':
-        form = UserRegistrationForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('login')
-        else:
-            form.add_error(None, 'Error in Registration')
-    else:
-        form = UserRegistrationForm()
-    return render(request, 'register.html', {'form': form})
+class UserRating:
+    pass
+
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AddRatingView(View):
+    def post(self, request):
+        data = json.loads(request.body)
+        user = get_object_or_404(User, id=data['user_id'])
+        product = get_object_or_404(Product, id=data['product_id'])
+        rating = data.get('rating')
+        review_title = data.get('review_title', '')
+        review_text = data.get('review_text', '')
+        verified_purchase = data.get('verified_purchase', False)
+
+        UserRating.objects.create(
+            user=user,
+            product=product,
+            rating=rating,
+            review_title=review_title,
+            review_text=review_text,
+            verified_purchase=verified_purchase
+        )
+
+        return JsonResponse({'message': 'Rating added successfully'}, status=201)
