518c5eb3addbf6e6f283c228ff38f066e9901890
juanitamelosha <meloshajuanita@gmail.com>
2024-07-18 02:50:13 -0400

    product model and main view created

---
+++ EcoFriendlyProducts/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
+from django.db import models
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    eco_certification = models.CharField(max_length=255)
+    category = models.CharField(max_length=100)
+    brand = models.CharField(max_length=100)
+    weight = models.DecimalField(max_digits=5, decimal_places=2)
+    dimensions = models.CharField(max_length=100)
+    material = models.CharField(max_length=255)
+    manufacturing_location = models.CharField(max_length=255)
+    packaging_type = models.CharField(max_length=255)
+    recyclable = models.BooleanField(default=False)
+    biodegradable = models.BooleanField(default=False)
+    cruelty_free = models.BooleanField(default=False)
+    energy_efficient = models.BooleanField(default=False)
+    image_url = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+from django.db import models
+
+# Create your models here.
---
+++ EcoFriendlyProducts/views.py
@@ -0,0 +1,41 @@
+import json
+import os
+from django.utils import timezone
+
+import pandas as pd
+
+from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import render
+
+from Eco_friendly_products import settings
+from .models import Product,
+from django.views import View
+
+
+
+
+def main(request):
+    today = timezone.now().date()
+    session_key = f"visited_{today}"
+    context = {
+        'products': Product.objects.order_by('-created_at')[:8],
+    }
+
+
+
+    if request.method == 'POST' and context['form'].is_valid():
+        data = context['form'].cleaned_data
+        file_path = os.path.join(settings.EXCEL_FILES_DIR, 'contacts.xlsx')
+        df = pd.read_excel(file_path) if os.path.exists(file_path) else pd.DataFrame(
+            columns=['name', 'email', 'phone', 'message'])
+        new_data = pd.DataFrame([data])
+        df = pd.concat([df, new_data], ignore_index=True)
+        df.to_excel(file_path, index=False)
+        return redirect('main')
+
+    return render(request, 'EcoFriendlyProducts/index.html', context)
+
+
+from django.shortcuts import render
+
+
