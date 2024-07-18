8bdddf22165b5cee922203b4923d23b3a63cfec8
Juanita Melosha K <juanitamelosha@meloshas-air.home>
2024-07-18 02:00:44 -0400

    product model and main view created

--- EcoFriendly/views.py
+++ EcoFriendly/views.py
@@ -20,28 +20,6 @@ def main(request):
         'products': Product.objects.order_by('-created_at')[:8],
         'form': ContactForm(request.POST or None)
     }
-
-    if request.user.is_authenticated:
-        user_visit, created = UserDailyVisit.objects.get_or_create(
-            user=request.user, date=today,
-            defaults={'visits': 0}
-        )
-        if not request.session.get(session_key):
-            user_visit.visits += 1
-            user_visit.save()
-            request.session[session_key] = True
-
-        # Context data only for authenticated users
-        context.update({
-            'user_visits_today': user_visit.visits,
-            'total_user_visits': UserDailyVisit.objects.filter(user=request.user).aggregate(Sum('visits'))[
-                                     'visits__sum'] or 0,
-            'total_visits_today': UserDailyVisit.objects.filter(date=today).aggregate(Sum('visits'))[
-                                      'visits__sum'] or 0,
-            'total_visits_all_time': UserDailyVisit.objects.aggregate(Sum('visits'))['visits__sum'] or 0,
-            'user': request.user  # Passing the user object to the template
-        })
-
     if request.method == 'POST' and context['form'].is_valid():
         data = context['form'].cleaned_data
         file_path = os.path.join(settings.EXCEL_FILES_DIR, 'contacts.xlsx')
@@ -55,30 +33,3 @@ def main(request):
     return render(request, 'EcoFriendlyProducts/index.html', context)
 
 
-def viewAllProducts(request):
-    # Query all products from the database
-    products = Product.objects.all()
-
-    # Pass the products to the template
-    context = {
-        'products': products
-    }
-
-    return render(request, 'EcoFriendlyProducts/shop.html',context)
-
-
-def shop(request):
-    query = request.GET.get('q')
-    if query:
-        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
-        no_results = not products.exists()  # Check if the filtered queryset is empty
-    else:
-        products = Product.objects.all()
-        no_results = False  # Always false when not searching
-
-    context = {
-        'products': products,
-        'query': query,
-        'no_results': no_results  # Pass this flag to the template
-    }
-    return render(request, 'EcoFriendlyProducts/shop.html', context)
